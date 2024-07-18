70b302b91763da9c76172f438664983c5e191a7f
LouisHouston <69780196+LouisHouston@users.noreply.github.com>
2024-07-17 22:57:58 -0700

    does this path work

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, "public\\views"));
+app.set('views', path.join(__dirname, 'public', "views"));
 
-app.use(express.static(path.join(__dirname, 'public\\views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { title: 'Date Checker', message: 'check?' });
